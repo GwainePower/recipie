1bd2ba5897fb774891aa7188559afe611484e2eb
ArtemiyBTR <artemiybtr@oxygen>
2022-07-07 03:19:34 +0300

    Recipe item styling part one

--- lib/ui/widgets/recipes/recipes_list_item.dart
+++ lib/ui/widgets/recipes/recipes_list_item.dart
@@ -11,17 +11,63 @@ class RecipesListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double imageBorderRadius = 20;
     final screenSize = MediaQuery.of(context).size;
-    return Container(
-      width: screenSize.width * 0.75,
-      height: screenSize.height * 0.3,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: screenSize.width,
+            height: screenSize.height * 0.3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(recipeItem.picture),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(imageBorderRadius),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: screenSize.height * 0.05,
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+                vertical: 4,
+              ),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  topLeft: Radius.circular(15),
+                ),
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: Text(
+                recipeItem.title,
+                style: const TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: screenSize.width,
+              height: screenSize.height * 0.08,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(imageBorderRadius),
+                  bottomRight: Radius.circular(imageBorderRadius),
+                ),
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+            ),
+          )
+        ],
       ),
-      child: Stack(children: [
-        Image.network(recipeItem.picture),
-        Text(recipeItem.title),
-      ]),
     );
   }
 }
